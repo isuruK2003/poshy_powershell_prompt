a64624ea517535424d2f6db8e48864a8c2cea049
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-08-02 00:39:37 +0530

    Delete profile.ps1

--- profile.ps1
+++
@@ -1,9 +0,0 @@
-function prompt {
-    $path = Split-Path -Path (Get-Location)
-    Write-Host "┌── " -ForegroundColor DarkBlue -NoNewline
-    Write-Host "PowerShell" -ForegroundColor Blue -NoNewline
-    Write-Host "@" -ForegroundColor DarkBlue -NoNewline
-    Write-Host "$path" -ForegroundColor Green -NoNewline
-    Write-Host "`n└─>" -ForegroundColor DarkBlue -NoNewline
-    return " "
-}
